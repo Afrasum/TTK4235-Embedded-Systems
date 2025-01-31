b9c63953ca554eb4bc83548440b082a02a5a5b0b
Afras <afras@hotmail.no>
2025-01-31 17:44:00 +0100

    fix: nested repo

---
+++ assignment_1/pipe.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main(){
+    printf("Welcome to TTK4235 - Embedded Systems\n");
+    printf("Please be aware of the fact that most animals, especially ferrets, are not allowed in Sanntidssalen.\n");
+    print("Linux er lett")
+
+    return 0;
+}
